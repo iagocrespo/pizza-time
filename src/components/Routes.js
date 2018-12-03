b45177183c0409cbd1dcbaaa10353c30adfa3970
Iago C <iagocrpi@gmail.com>
2018-12-03 08:39:46 +0100

    Añadido el componente <Pizza> con el path `secret-dinner`
    Creamos el componente <Pizza> en Routes y podemos acceder a el gracias a la ruta que le ponemos en el path `secret-dinner`

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -3,11 +3,13 @@ import { Router } from "@reach/router";
 import StorePicker from "./StorePicker";
 import App from "./App";
 import NotFound from "./NotFound";
+import Pizza from "./Pizza";
 
 const Routes = () => (
   <Router>
     <StorePicker path="/" />
     <App path="/store/:storeId" />
+    <Pizza path="/secret-dinner" />
     {/* TODO: Hook up the new Pizza component to render @ "/secret-dinner" */}
     {/* HINT: You need to write code 👇 and also 👆 */}
     <NotFound default />
